7a74e2b841cac94b43b6ed2ced403444f1c60a3f
jdvargassanchez <vjuandavid777@gmail.com>
2026-03-23 14:13:11 -0500

    Add Use Case 1 - House Price Prediction

--- app.py
+++ app.py
@@ -1,18 +1,13 @@
 from flask import Flask, render_template, request
 import LinearRegression
-import LogisticRegressionModel
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello Flask"
-
-@app.route('/FirstPage')
-def firstPage():
     return render_template('index.html')
 
-@app.route('/LineaRegression', methods=["GET", "POST"])
+@app.route('/linear-regression', methods=["GET", "POST"])
 def calculateGrade():
     calculateResult = None
     if request.method == "POST":
@@ -20,19 +15,6 @@ def calculateGrade():
         calculateResult = LinearRegression.calculateGrade(hours)
     return render_template("linearRegressionGrades.html", result = calculateResult)
 
-@app.route('/logisticRegression', methods=["GET", "POST"])
-def logisticRegression():
-    result = None
-    
-    if request.method == "POST":
-        edad = float(request.form["edad"])
-        ingreso = float(request.form["ingreso"])
-        visitas = float(request.form["visitas"])
-        tiempo = float(request.form["tiempo"])
-        compras = float(request.form["compras"])
-        descuento = float(request.form["descuento"])
-
-        data = [edad, ingreso, visitas, tiempo, compras, descuento]
-        result = LogisticRegressionModel.predict(data)
-
-    return render_template("logisticRegression.html", result=result)
+@app.route('/use-case1')
+def use_case1():
+    return render_template('use_cases1.html')
