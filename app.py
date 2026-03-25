6675cdc9089f75458f21847a752b7d308a872228
camilo sichaca <kmilocanon105@gmail.com>
2026-03-24 20:38:08 -0500

    Create the regression line and add code, and also agree on data sets to work with them.

--- app.py
+++ app.py
@@ -22,3 +22,11 @@ def linear_app():
 @app.route('/use-case2')
 def use_case2():
     return render_template('use_cases2.html')
+
+@app.route('/use-case3')
+def use_case3():  
+    return render_template('use_cases3.html')
+
+@app.route('/use-case4')
+def use_case4():
+    return render_template('use_cases4.html')
