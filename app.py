e04e27a6f24c3365d66bc37bbf724fc62d322e9c
camilo sichaca <kmilocanon105@gmail.com>
2026-03-28 11:05:49 -0500

    Modify the logistic regression path to call the predict function and avoid having logistic functions in the paths app

--- app.py
+++ app.py
@@ -55,25 +55,7 @@ def salary_app():
 def logistic_app():
     prediction = None
     if request.method == 'POST':
-        age = float(request.form['age'])
-        gender = float(request.form['gender'])
-        vaccination_status = float(request.form['vaccination_status'])
-        fever = float(request.form['fever'])
-        cough = float(request.form['cough'])
-        fatigue = float(request.form['fatigue'])
-        shortness_of_breath = float(request.form['shortness_of_breath'])
-        loss_of_smell = float(request.form['loss_of_smell'])
-        headache = float(request.form['headache'])
-        diabetes = float(request.form['diabetes'])
-        hypertension = float(request.form['hypertension'])
-        heart_disease = float(request.form['heart_disease'])
-        asthma = float(request.form['asthma'])
-        cancer = float(request.form['cancer'])
-        prediction = logisticregressionexample.get_prediction(
-            age, gender, vaccination_status, fever, cough, fatigue,
-            shortness_of_breath, loss_of_smell, headache, diabetes,
-            hypertension, heart_disease, asthma, cancer
-        )
+        prediction = logisticregressionexample.handle_form(request.form)
     return render_template('logistic_regression_app.html', prediction=prediction)
 
 if __name__ == '__main__':
