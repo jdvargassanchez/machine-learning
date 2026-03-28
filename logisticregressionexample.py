7c6afdc3977abed3c72237fbccd57b7839034978
camilo sichaca <kmilocanon105@gmail.com>
2026-03-28 09:46:01 -0500

    create a new file called logisticregression.py and import all of requeriments  for logistic regresssion

---
+++ logisticregressionexample.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score, confusion_matrix, classification_report
+from sklearn.preprocessing import StandardScaler
+from sklearn.datasets import make_classification
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+
