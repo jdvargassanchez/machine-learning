0435279d3030da9f2cc52c8a7cabae5763a367d4
Camilo Sichaca <kmilocanon105@gmail.com>
2026-04-24 18:27:41 -0500

    rename the dataset

--- Clustering.py
+++ Clustering.py
@@ -5,22 +5,19 @@ import pandas as pd
 import numpy as np
 
 def getdataset():
-    return [ {"nombre": "Ana", "edad": 22, "ingresos": 1200, "gasto": 300},
-    {"nombre": "Luis", "edad": 25, "ingresos": 1500, "gasto": 350},
-    {"nombre": "Carlos", "edad": 23, "ingresos": 1300, "gasto": 280},
-    {"nombre": "Marta", "edad": 45, "ingresos": 4000, "gasto": 1200},
-    {"nombre": "Sofía", "edad": 50, "ingresos": 4200, "gasto": 1400},
-    {"nombre": "Jorge", "edad": 47, "ingresos": 3900, "gasto": 1100},
-    {"nombre": "Elena", "edad": 31, "ingresos": 2500, "gasto": 700},
-    {"nombre": "Pedro", "edad": 33, "ingresos": 2700, "gasto": 750},
-    {"nombre": "Laura", "edad": 29, "ingresos": 2400, "gasto": 680},
-    {"nombre": "Andrés", "edad": 52, "ingresos": 5000, "gasto": 1600},
-    {"nombre": "Camila", "edad": 21, "ingresos": 1100, "gasto": 250},
-    {"nombre": "Diego", "edad": 38, "ingresos": 3200, "gasto": 900}]
+    df = pd.read_csv("credit_card_dataset.csv")
+    df = df.dropna()
+    data = df.to_dict(orient="records")
+
+    return data
 
 def ApplyClusteringKmeans():
     data =getdataset()
-    x=[[person["edad"],person["ingresos"],person["gasto"]] for person in data]
+    x = [[
+        person["BALANCE"],
+        person["PURCHASES"],
+        person["CREDIT_LIMIT"]
+    ] for person in data]
 
     scaler=StandardScaler()
     Xscaled=scaler.fit_transform(x)
